9da4b8532461745e7b9f0b18ea6ef16f6dfcf28d
tataqt <tata_98@ukr.net>
2020-04-18 14:28:45 +0300

    add spinner

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+const { v4 } = require('uuid');
 const app = express();
 
 const CONTACTS = [
-    { id: 1, name: "tata", value: '3801002030', marked: false }
+    { id: v4(), name: "tata", value: '3801002030', marked: false }
 ]
 
 //GET
@@ -11,10 +12,15 @@ app.get('/api/contacts', (req, res) => {
     res.status(200).json(CONTACTS);
 })
 
+//POST
+app.post('/api/contacts', (req, res) => {
+    res.status(200).json(CONTACTS);
+})
+
 app.use(express.static(path.resolve(__dirname, 'client')));
 
 app.get('*', (req, res) => {
-    res.sendfile(path.resolve(__dirname, 'client', 'index.html'));
+    res.res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
 });
 
 app.listen(3000, () => console.log('Server has been started on port 3000...'))
--- client/frontend.js
+++ client/frontend.js
@@ -1,9 +1,20 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
+Vue.component('loader', {
+    template: `
+    <div style="display:flex; justify-content:center;align-items:center">
+        <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+  `
+});
+
 new Vue({
     el: '#app',
     data() {
         return {
+            loading: false,
             form: {
                 name: '',
                 value: ''
@@ -31,8 +42,9 @@ new Vue({
         },
     },
     async mounted() {
-        this.contacts  = await request('/api/contacts');
-        
+        this.loading = true;
+        this.contacts = await request('/api/contacts');
+        this.loading = false;
     }
 })
 
